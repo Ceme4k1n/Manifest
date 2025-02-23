e4775af7ab199774eba91bdc9b7b23d80f5e2c1a
Mac <mac@MacBook-Air-Mac.local>
2025-02-23 15:05:44 +0300

    Delete: Seed-fraze, crypto

--- src/controllers/authControlles.ts
+++ src/controllers/authControlles.ts
@@ -1,11 +1,9 @@
 import express, { Express, Request, Response } from 'express'
-import crypto from 'crypto'
-import * as bip39 from '@scure/bip39'
-import { wordlist } from '@scure/bip39/wordlists/english'
 const dotenv = require('dotenv')
 import jwt from 'jsonwebtoken'
 dotenv.config()
 import db from '../database/db'
+
 const SECRET_KEY = process.env.SECRET_KEY || 'test'
 const SECRET_SALT = process.env.SECTER_SATL || 'test'
 
@@ -21,7 +19,6 @@ export const user_login = async (req: Request, res: Response) => {
       }
 
       const token = jwt.sign({ name: email }, SECRET_KEY, { expiresIn: '5s' })
-
       res.status(200).json({ message: 'Логин успешный', token })
       console.log(user)
     } catch (error) {
@@ -30,6 +27,7 @@ export const user_login = async (req: Request, res: Response) => {
     }
   }
 }
+
 // 2. POST: Регистрация пользователя
 export const user_register = async (req: Request, res: Response) => {
   const { name, lastName, email, password } = req.body
@@ -46,33 +44,13 @@ export const user_register = async (req: Request, res: Response) => {
         const futureId = 'id_' + (result.max + 1)
         console.log(futureId)
 
-        const entropeBytes = crypto.randomBytes(16)
-        const userHash = crypto
-          .createHash('sha256')
-          .update(futureId + SECRET_SALT)
-          .digest()
-        const entropyFinal = Buffer.from(entropeBytes.map((byte, i) => byte ^ userHash[i % userHash.length]))
-        const seefFraz = bip39.entropyToMnemonic(entropyFinal, wordlist)
-        console.log('Финал энтропии: ', entropyFinal)
-        console.log('Финальная фраза:', seefFraz)
-
-        decod(seefFraz)
-        //await db.none('INSERT INTO man.users(username, email, password_hash, fio) VALUES($1, $2, $3, $4)', [futureId, email, password, fio])
-        // const token = jwt.sign({ email: email }, SECRET_KEY, { expiresIn: '5s' })
-        // res.json({ message: `Рега успешная`, token })
-        // console.log('Юзер создан, с таким email: ', email)
+        await db.none('INSERT INTO man.users(username, email, password_hash, fio) VALUES($1, $2, $3, $4)', [futureId, email, password, fio])
       }
     } catch (error) {
       console.error(error)
       res.status(501).json({ error: 'Database error' })
     }
   }
-}
-
-const decod = (fraze: string) => {
-  const users = ['id_72', 'id_73']
-  const ent = Buffer.from(bip39.mnemonicToEntropy(fraze, wordlist))
-  console.log('Фраза в цифры:', ent)
 }
 
 // 3. GET: Проверка JWT Token-a
